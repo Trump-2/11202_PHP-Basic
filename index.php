56693db883e92c3efe56294969a6e4eb06683115
YIHONG <akashidaiki2@gmail.com>
2023-10-02 10:32:36 +0800

    變數

---
+++ index.php
@@ -0,0 +1,40 @@
+<!-- <!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="style.css" rel="stylesheet">
+  <title>Document</title>
+
+</head>
+
+<body> -->
+<?php
+  $age=300;
+  echo $age;
+  $name="WUYIHONG";
+  echo $name;
+
+  // function test () {
+  //   print $name;
+  // }
+
+  // 宣告常數
+  // define('lastName',132);
+  // echo 'lastName';
+
+  // 將兩變數的值交換
+  $a = 10;
+  $b = 50;
+  $c = $a;
+  $a = $b;
+  $b = $c;
+  print($a);
+  print($b);
+  
+?>
+
+<!-- </body>
+
+</html> -->
---
+++ test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+let age = 10;
+
+const calcAge = function() {
+  console.log(age)
+}
