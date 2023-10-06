017f6e3783602e8983bba25d10346657eb2fca83
YIHONG <akashidaiki2@gmail.com>
2023-10-06 14:07:39 +0800

    多維陣列存取

--- array_20231006.php
+++ array_20231006.php
@@ -13,6 +13,7 @@ echo "<pre>";
 print_r($a);
 echo "</pre>";
 
+
 echo $a[0];
 echo "<br>";
 echo $a[2];
@@ -27,3 +28,22 @@ echo "</pre>";
 echo $b["age"];
 echo "<br>";
 echo $b["phone"];
+
+echo "<hr>";
+$c = [];
+$c['name'] = "Wu";
+$c['hobby'] = ['a' => 'amime', 'b' => 'coding', 'c' => 'technology', 'd' => 'become strong'];
+echo "<pre>";
+print_r($c);
+echo "</pre>";
+
+echo $c['hobby']['b'];
+
+$d = [];
+$d['name'] = "Wu";
+$d['hobby'] = ['amime', 'coding', 'technology',  'become strong'];
+echo "<pre>";
+print_r($d);
+echo "</pre>";
+
+echo $d['hobby'][0];
