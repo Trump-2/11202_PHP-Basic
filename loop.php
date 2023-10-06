6dc80b051c4cac3e721e737045a106af891610b3
YIHONG <akashidaiki2@gmail.com>
2023-10-06 11:01:09 +0800

    for 迴圈題目練習

--- loop.php
+++ loop.php
@@ -6,3 +6,48 @@ for ($i = 1; $i <= 100; $i = $i + 1) {
 }
 
 echo "1 加到 100 的結果是 " . $sum;
+
+?>
+
+<h1>for 練習</h1>
+<h3>使用for迴圈來產生以下的數列</h3>
+<ul>
+  <li>1,3,5,7,9……n</li>
+  <li>10,20,30,40,50,60……n</li>
+  <li>3,5,7,11,13,17……97</li>
+</ul>
+<?php
+
+$n = 200;
+for ($i = 1; $i <= 100; $i += 2) {
+  // echo $i . ',';
+  echo "$i,";
+}
+echo "<hr>";
+for ($i = 10; $i <= $n; $i += 10) {
+  echo "$i,";
+}
+echo "<hr>";
+for ($i = 1; $i <= $n; $i++) {
+  echo ($i * 10) . ",";
+}
+
+echo "<hr>";
+$a = 18;
+$flag = true;
+// %a / 2 => 能讓迴圈執行的次數再減少
+for ($i = 2; $i < ($a / 2); $i++) {
+  echo "$a 除以 $i 的餘數 " . ($a % $i);
+  if ($a % $i === 0) {
+    $flag = false;
+    break;
+  }
+  echo "<br>";
+}
+
+echo "<br>";
+
+if ($flag === true)
+  echo "$a" . "是質數";
+else
+  echo "$a " . "不是質數";
--- while.php
+++ while.php
@@ -1,11 +1,33 @@
 <?php
 
-$sum2 = 0;
+$sum = 0;
 $i = 1;
 while ($i <= 100) {
-  $sum2 += $i;
+  $sum += $i;
 
   $i++;
 }
 
-echo "1 加到 100 的結果是 " . $sum2;
+echo "1 加到 100 的結果是 " . $sum;
+echo "<hr>";
+
+
+$sum = 0;
+$i = 1;
+
+while ($i <= 100) {
+  // echo '當$i=' . $i . "時";
+  // echo '$sum+$i的結果是';
+  // echo $sum . " + " . $i . "=";
+  // echo $sum + $i;
+  // echo "<br>";
+
+  echo '當$i = 1 時, $sum + $i 的結果為：';
+  echo "$sum" . "+" . $i . "=";
+
+  // 將目前的數字加到總和中
+  $sum += $i;
+
+  // 增加計數器
+  $i++;
+}
