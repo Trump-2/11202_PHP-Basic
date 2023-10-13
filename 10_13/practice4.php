8623e56861f0da868ce277d99423d259d55223c1
YIHONG <akashidaiki2@gmail.com>
2023-10-13 14:11:03 +0800

    畫出倒正三角形

--- 10_13/practice4.php
+++ 10_13/practice4.php
@@ -35,12 +35,24 @@ echo "<br>";
 ?>
 
 <style>
-  * {
-    font-family: 'Courier New', Courier, monospace
-  }
+* {
+  font-family: 'Courier New', Courier, monospace
+}
 </style>
 <?php
 for ($i = 0; $i < 5; $i++) {
+  for ($j = 0; $j < (4 - $i); $j++) {
+    echo "&nbsp";
+  }
+  for ($k = 0; $k < (2 * $i + 1); $k++) {
+    echo "*";
+  }
+  echo "<br>";
+}
+?>
+<h2>倒正三角形</h2>
+<?php
+for ($i = 4; $i >= 0; $i--) {
   for ($j = 0; $j < (4 - $i); $j++) {
     echo "&nbsp";
   }
