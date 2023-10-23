f4aaac2b493aaaf465efd4dca05491d19cad1da4
YIHONG <akashidaiki2@gmail.com>
2023-10-23 14:41:08 +0800

    反轉陣列，但不創造新陣列的前提下

--- 10_23/array_pra04.php
+++ 10_23/array_pra04.php
@@ -1,11 +1,22 @@
 <h1>找出五百年內的閏年</h1>
 <ul>
-  請依照閏年公式找出五百年內的閏年
-  使用陣列來儲存閏年
-  使用迴圈來印出閏年
-  <li>
+  <li>請依照閏年公式找出五百年內的閏年</li>
+  <li>使用陣列來儲存閏年</li>
+  <li>使用迴圈來印出閏年</li>
 </ul>
-</li>
+
 
 
 <?php
+$leapYears = [];
+for ($year = 2023; $year <= 2523; $year++) {
+  if ($year % 4 === 0 && ($year % 100 !== 0 || $year % 400 === 0)) {
+    $leapYears[] = $year;
+  }
+}
+
+foreach ($leapYears as $each) {
+  echo $each . ", ";
+}
+echo "<br>";
+echo count($leapYears);
